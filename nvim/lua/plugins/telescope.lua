0afdce3651d49701b7630859a9e3382b617c4082
nolfonzo <nolfonzo@gmail.com>
2024-05-19 00:30:08 +1000

    initial, grouped together all config

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,7 @@
+return {
+  -- plugins/telescope.lua:
+  'nvim-telescope/telescope.nvim', tag = '0.1.6',
+  -- or                              , branch = '0.1.x',
+   dependencies = { 'nvim-lua/plenary.nvim' }
+ }
+ 
