b2fdd4d94c596572dd79352c35777fcbff6b454f
nolfonzo <nolfonzo@gmail.com>
2025-05-21 20:42:31 +1000

    vim updates

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,13 @@
 return {
-  -- plugins/telescope.lua:
-  'nvim-telescope/telescope.nvim', tag = '0.1.6',
-  -- or                              , branch = '0.1.x',
-   dependencies = { 'nvim-lua/plenary.nvim' }
- }
- 
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    config = function()
+      require("telescope").load_extension("fzf")
+    end,
+  },
+}
